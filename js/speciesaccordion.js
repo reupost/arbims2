743b49a82ab6b5afdf7b872dc2beca9fd1b8a391
Reuben Roberts <reupost@users.noreply.github.com>
2020-03-09 10:07:42 +0000

    fixes to taxon accordion

--- js/speciesaccordion.js
+++ js/speciesaccordion.js
@@ -1,6 +1,7 @@
 $("html").addClass("js");
 $.fn.accordion.defaults.container = false;
 var already_loaded = new Object();  // used to track which accordions have already been loaded
+var expanded = new Object(); //used to track open/closed display of node; this should be do-able directly but loading subnodes through ajax seems to have broken the accordion behaviour
 
 $(function() {
     $("#species_accordion").accordion({        
@@ -9,7 +10,6 @@ $(function() {
         retFunc: function(treeUrl) {
             var node_link = $(treeUrl.context).attr('href');
             if(! already_loaded[node_link] == 1) {
-                console.log(node_link);
                 var treeContext = treeUrl.context.innerHTML.split(" ");
                 var tRank = treeContext[0].replace(":", ""); //TODO: this is horrible
                 var tEpithet = treeContext[1].replace("(unnamed)", "");
@@ -17,11 +17,23 @@ $(function() {
                 if (node_link.indexOf("#") > 0) {
                     curLink = node_link.substring(node_link.indexOf("#") + 1);
                 }
-                var urlTree = "data.taxontree.php?region=albertine&taxon_epithet=" + tEpithet + "&rank=" + tRank + "&use_backbone=true&use_other=true&use_occ=false&cur_link=" + curLink;
+                var urlTree = "data.taxontree.php?region=" + REGION + "&taxon_epithet=" + tEpithet + "&rank=" + tRank + "&use_backbone=true&use_occ=false&cur_link=" + curLink;
                 $.get(urlTree).success(function (data) {
                     treeUrl.next().after(data);
                     already_loaded[node_link] = 1; // keep track of the loaded accordions
+                    expanded[node_link] = 1;
                 });
+            } else {
+                if (expanded[node_link] == 1) {
+                    $(treeUrl).removeClass("open");
+                    $(treeUrl).siblings('ul').hide();
+                    expanded[node_link] = 0;
+                } else {
+                    $(treeUrl).addClass("open");
+                    $(treeUrl).siblings('ul').show();
+                    expanded[node_link] = 1;
+                }
+
             }
         }
     });
@@ -43,14 +55,14 @@ $(function() {
         var selector = "a[href$='#" + node + "']";
         var link = $(selector);
         if (link) {
-            console.log(link[0].text);
+            console.log('loading ' + link[0].text);
             $(link[0]).addClass('open');
             var treeContextTxt = link[0].text.toString();
             var treeContext = treeContextTxt.split(" ");
             var tRank = treeContext[0].replace(":", ""); //TODO: this is horrible
             var tEpithet = treeContext[1].replace("(unnamed)", "");
             var curLink = node;
-            var urlTree = "data.taxontree.php?region=albertine&taxon_epithet=" + tEpithet + "&rank=" + tRank + "&use_backbone=true&use_other=true&use_occ=false&cur_link=" + curLink;
+            var urlTree = "data.taxontree.php?region=" + REGION + "&taxon_epithet=" + tEpithet + "&rank=" + tRank + "&use_backbone=true&use_occ=false&cur_link=" + curLink;
 
             $.ajax({
                 url: urlTree,
--- templates/species.tpl.php
+++ templates/species.tpl.php
@@ -1,4 +1,7 @@
 <legend><?php echo getMLtext('species_explorer') . ': ' . getMLtext('region_' . $region) ?></legend>
+<script type='text/javascript'>
+    var REGION = "<?php echo $region ?>";
+</script>
 <div class="row-fluid">
     <div class="span12">
         <div class='help_prompt'><?php printMLtext('arbmis_species') ?></div>
